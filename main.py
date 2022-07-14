8d00852fb54bb1337055327bff662dce7d8c8e67
kristigordon <kristimariegordon@gmail.com>
2022-07-14 17:29:04 -0400

    BandNameGenerator

---
+++ main.py
@@ -0,0 +1,6 @@
+print('Welcome to the Band Name Generator App')
+city = input('Where did you grow up?\n')
+pet = input('What was the name of your first pet?\n')
+print("You should rock out as" + ' ' + city + ' ' + pet)
+
+
